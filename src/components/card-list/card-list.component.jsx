1842211b4232b2cf441901b68c433b239e69f1b7
Kirill Arkhypov <k.arkhypov@gmail.com>
2022-04-28 21:47:41 +0300

    basic version implemented

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+import { Component } from 'react';
+import Card from '../card/card.component';
+
+import './card-list.styles.css';
+
+class CardList extends Component {
+  render() {
+    const { monsters } = this.props;
+
+    return (
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          const { id, name, email } = monster;
+
+          return <Card id={id} name={name} email={email} />;
+        })}
+      </div>
+    );
+  }
+}
+
+export default CardList;
